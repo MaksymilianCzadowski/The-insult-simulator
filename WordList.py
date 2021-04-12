ee9d25357d691f35e69a7b4e983d377853d385a8
MROADLEY-BATTIN <maxime.roadleybattin@ynov.com>
2021-04-12 16:49:16 +0200

    faiblesses :)

--- WordList.py
+++ WordList.py
@@ -1,7 +1,7 @@
 class listword:
     sujet = ["Tu", "Ton chapeau", "Ton costume", "Tes cheveux", "Ton crâne", "Ton nez", "Ton fessier",
              "Ton postérieur", "Ta maman", "Ton père", "Ta famille", "Ta soeur", "Ton frère", "Ta grand-mère",
-             "Tes enfants", "Ton fils", "Ta fille", "ta tante", "Tes yeux", "Tes jambes", "Tes mains",
+             "Tes enfants", "Ton fils", "Ta fille", "Ta tante", "Tes yeux", "Tes jambes", "Tes mains",
              "Ta progéniture", "Tes ancêtres", "Tes morts", "Ton pantalon", "Tes chaussures", "Ton ithyphalle", "Ta femme", "Ton marie"]
 
     verbe = ["est/sont", "a/ont", "ressemble(nt)", "parle(nt)", "andocite(nt)", "substante(nt)", "mange(nt)",
@@ -9,11 +9,11 @@ class listword:
 
     complement = ["un caca", "du pipi", "un chauve", "un avion de chasse", "un arbre", "une truite", "un saumon",
                   "un homme sauvage", "un carré à trois côtés ", "un train ", "une jument prématurée", "faut-cul",
-                  "puceau", "Faquin", "orchidoclaste", "tête de têtard", "sac à puces", "patate douce", "une fusée",
+                  "puceau", "faquin", "orchidoclaste", "tête de têtard", "sac à puces", "patate douce", "une fusée",
                   "le soleil", "la face de la lune caché", "un cornichon des mers", "un Parisien", "un Breton",
                   "un Marseillais", "un Ch'ti", "les anges de la téléréalité", "Nabila", "fils/fille de colon",
                   "un envahisseur", "un parachute", "fumeur", "balai à chiotte", "un déchet non recyclable",
-                  "fond de benne", "Internet explorer", "PNJ mal codé"]
+                  "fond de benne", "Internet explorer", "PNJ mal codé", "bourgeois"]
 
     conjonction = ["mais", "ou", "et", "donc", "or", "ni", "car", "à", "dans", "par", "pour", "avec", "de", "sans",
                    "sous", "entre", "derrière", "chez", "en", "vers"]
