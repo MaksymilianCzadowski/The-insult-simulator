a30dab2cc6468000b9ee49466a1729969d7a6c0c
MROADLEY-BATTIN <maxime.roadleybattin@ynov.com>
2021-04-12 16:16:28 +0200

    faiblesses veixu + jeune

--- WordList.py
+++ WordList.py
@@ -1,6 +1,6 @@
 class listword:
     sujet = ["Tu", "Ton chapeau", "Ton costume", "Tes cheveux", "Ton crâne", "Ton nez", "Ton fessier",
-             "Ton postérieur", "Ta maman", "Ton père", "Ta fammile", "Ta soeur", "Ton frère", "Ta grand-mère",
+             "Ton postérieur", "Ta maman", "Ton père", "Ta famille", "Ta soeur", "Ton frère", "Ta grand-mère",
              "Tes enfants", "Ton fils", "Ta fille", "ta tante", "Tes yeux", "Tes jambes", "Tes mains",
              "Ta progéniture", "Tes ancêtres", "Tes morts", "Ton pantalon", "Tes chaussures", "Ton ithyphalle", "Ta femme", "Ton marie"]
 
@@ -11,9 +11,9 @@ class listword:
                   "un homme sauvage", "un carré à trois côtés ", "un train ", "une jument prématurée", "faut-cul",
                   "puceau", "Faquin", "orchidoclaste", "tête de têtard", "sac à puces", "patate douce", "une fusée",
                   "le soleil", "la face de la lune caché", "un cornichon des mers", "un Parisien", "un Breton",
-                  "un Marseillé", "un Ch'ti", "les anges de la téléréalité", "Nabila", "fils/fille de colon",
+                  "un Marseillais", "un Ch'ti", "les anges de la téléréalité", "Nabila", "fils/fille de colon",
                   "un envahisseur", "un parachute", "fumeur", "balai à chiotte", "un déchet non recyclable",
-                  "fond de benne", "internet explorer", "PNJ mal codé"]
+                  "fond de benne", "Internet explorer", "PNJ mal codé"]
 
     conjonction = ["mais", "ou", "et", "donc", "or", "ni", "car", "à", "dans", "par", "pour", "avec", "de", "sans",
                    "sous", "entre", "derrière", "chez", "en", "vers"]
