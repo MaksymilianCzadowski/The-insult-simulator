ec9c944f182e0f999ed57c74794439405a889e8e
MROADLEY-BATTIN <maxime.roadleybattin@ynov.com>
2021-04-12 16:49:35 +0200

    faiblesses :)

---
+++ Personnages/Intello.py
@@ -0,0 +1,5 @@
+class intello:
+    def __init__(self):
+        self.faiblesse = ["Tu", "Ta maman", "Ton père", "Ta soeur", "Ta tante", "Ton frère", "Ta grand-mère",
+                          "Ta famille", "Ta femme", "Ton marie", "un avion de chasse", "une truite", "un homme sauvage"
+                          ,"un carré à trois côtés", "puceau", "tête de têtard", "Nabila", "PNJ mal codé"]
