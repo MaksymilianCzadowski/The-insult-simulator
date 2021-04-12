b58384eceb6a5d188e811d31f1e5806804970ec6
MROADLEY-BATTIN <maxime.roadleybattin@ynov.com>
2021-04-12 16:29:06 +0200

    faiblesses veixu + jeune

--- Personnages/Ancien.py
+++ Personnages/Ancien.py
@@ -2,4 +2,4 @@ class ancien:
     def __init__(self):
         self.faiblesse = ["Tes cheveux", "Ton crâne", "Tes enfants", "Ta fille", "Ton fils", "Ta progéniture",
                           "Ta famille", "un chauve", "balai à chiotte", "un déchet non recyclable", "fond de benne",
-                          "Internet explorer", "prout"]
+                          "Internet explorer"]
--- Personnages/Jeune.py
+++ Personnages/Jeune.py
@@ -2,4 +2,4 @@ class jeune:
     def __init__(self):
         self.faiblesse = ["Tes cheveux", "Ton crâne", "Ton nez", "Ton fessier", "Ton postérieur", "Tes yeux",
                           "Tes jambes", "Tes mains", "Ton pantalon", "Tes chaussures", "puceau", "tête de têtard",
-                          "un Marseillais", "les anges de la téléréalité", "Nabila", "prout"]
+                          "un Marseillais", "les anges de la téléréalité", "Nabila"]
